2cbcaeb28134d60495a8556857fe9e0968eac8bc
elmiado <sayad.mahdi75@gmail.com>
2023-01-11 16:26:42 +0330

    add src files

---
+++ src/constants/constants.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: 'Web Administrator at Iranfarda Co.',
+    description: "It has always been a pleasure for me to work in an environment where cultural goals are " +
+        "one of the company's goals. I worked as an e-commerce manager and ensured that the platform performs " +
+        "well in all technical and SEO fields.",
+      image: '/images/iranfarda3.png',
+      tags: ['Javascript', 'HTML/CSS', 'React'],
+    source: 'https://www.iranefarda.com/',
+    visit: 'https://www.iranefarda.com/',
+    id: 0,
+  },
+  {
+    title: 'CTO & Founder at Kavosh Co.',
+    description:"we founded our first company with my brother in concurrency industry where we were trying to create " +
+        "and define profitable strategies for hedging and asset managements, I worked with a lot of software engenders " +
+        "and learn a lot about the start up community and the nature of computer science",
+    image: '/images/kavosh logo-01.png',
+    tags: ['Algorithmic Trading', 'Python', 'Design Patterns'],
+    source: 'https://www.linkedin.com/company/kavoshalgo/',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  // {
+  //   title: '',
+  //   description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //     image: '/images/3.jpg',
+  //     tags: ['React', 'WebRTC'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 2,
+  // },
+  // {
+  //   title: 'Unichat',
+  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: '/images/4.jpg',
+  //   tags: ['React', 'ChatEngine', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 3,
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Worked at Iranfarda Publisher Co. as Web Administrator', },
+  { year: 2020, text: 'Founded our first Algorithm Trading Co. with my brother', },
+  { year: 2021, text: 'Scaling our company, establishing our headquarter and Teams', },
+  { year: 2022, text: 'Moving our business to canada for scaling', },
+  { year: 2023, text: 'Epicure', },
+];
